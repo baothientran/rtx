1064c3b4416371ed5acda24d44123a48e5c051db
Bao Tran <baotran@Baos-MacBook-Pro.local>
2020-12-06 12:52:40 -0500

    remove target dir

--- rtx/target/debug/build/getrandom-ccf05288a1fda01e/build_script_build-ccf05288a1fda01e.d
+++
@@ -1,5 +0,0 @@
-/Users/baotran/Projects/rtx/rtx/target/debug/build/getrandom-ccf05288a1fda01e/build_script_build-ccf05288a1fda01e: /Users/baotran/.cargo/registry/src/github.com-1ecc6299db9ec823/getrandom-0.1.15/build.rs
-
-/Users/baotran/Projects/rtx/rtx/target/debug/build/getrandom-ccf05288a1fda01e/build_script_build-ccf05288a1fda01e.d: /Users/baotran/.cargo/registry/src/github.com-1ecc6299db9ec823/getrandom-0.1.15/build.rs
-
-/Users/baotran/.cargo/registry/src/github.com-1ecc6299db9ec823/getrandom-0.1.15/build.rs:
--- rtx/target/debug/deps/ppv_lite86-e160d7810aafcc5a.d
+++
@@ -1,9 +0,0 @@
-/Users/baotran/Projects/rtx/rtx/target/debug/deps/ppv_lite86-e160d7810aafcc5a.rmeta: /Users/baotran/.cargo/registry/src/github.com-1ecc6299db9ec823/ppv-lite86-0.2.10/src/lib.rs /Users/baotran/.cargo/registry/src/github.com-1ecc6299db9ec823/ppv-lite86-0.2.10/src/soft.rs /Users/baotran/.cargo/registry/src/github.com-1ecc6299db9ec823/ppv-lite86-0.2.10/src/types.rs /Users/baotran/.cargo/registry/src/github.com-1ecc6299db9ec823/ppv-lite86-0.2.10/src/x86_64/mod.rs /Users/baotran/.cargo/registry/src/github.com-1ecc6299db9ec823/ppv-lite86-0.2.10/src/x86_64/sse2.rs
-
-/Users/baotran/Projects/rtx/rtx/target/debug/deps/ppv_lite86-e160d7810aafcc5a.d: /Users/baotran/.cargo/registry/src/github.com-1ecc6299db9ec823/ppv-lite86-0.2.10/src/lib.rs /Users/baotran/.cargo/registry/src/github.com-1ecc6299db9ec823/ppv-lite86-0.2.10/src/soft.rs /Users/baotran/.cargo/registry/src/github.com-1ecc6299db9ec823/ppv-lite86-0.2.10/src/types.rs /Users/baotran/.cargo/registry/src/github.com-1ecc6299db9ec823/ppv-lite86-0.2.10/src/x86_64/mod.rs /Users/baotran/.cargo/registry/src/github.com-1ecc6299db9ec823/ppv-lite86-0.2.10/src/x86_64/sse2.rs
-
-/Users/baotran/.cargo/registry/src/github.com-1ecc6299db9ec823/ppv-lite86-0.2.10/src/lib.rs:
-/Users/baotran/.cargo/registry/src/github.com-1ecc6299db9ec823/ppv-lite86-0.2.10/src/soft.rs:
-/Users/baotran/.cargo/registry/src/github.com-1ecc6299db9ec823/ppv-lite86-0.2.10/src/types.rs:
-/Users/baotran/.cargo/registry/src/github.com-1ecc6299db9ec823/ppv-lite86-0.2.10/src/x86_64/mod.rs:
-/Users/baotran/.cargo/registry/src/github.com-1ecc6299db9ec823/ppv-lite86-0.2.10/src/x86_64/sse2.rs:
