b331de089b40f0b4873cb7ed7fe8bb4fc5409965
Bao Tran <baotran@Baos-MacBook-Pro.local>
2020-12-13 01:53:23 -0500

    add ops overload for mat4 and test

--- rtx/src/core/mat4.rs
+++ rtx/src/core/mat4.rs
@@ -107,6 +107,109 @@ impl Mat4 {
     }
 }
 
+impl ops::Add<Mat4> for Mat4 {
+    type Output = Mat4;
+
+    fn add(self, rhs: Mat4) -> Mat4 { 
+        return Mat4{
+            cols: [
+                self.cols[0] + rhs.cols[0],
+                self.cols[1] + rhs.cols[1],
+                self.cols[2] + rhs.cols[2],
+                self.cols[3] + rhs.cols[3],
+            ] 
+        } ;
+    }
+}
+
+impl ops::Sub<Mat4> for Mat4 {
+    type Output = Mat4;
+
+    fn sub(self, rhs: Mat4) -> Mat4 { 
+        return Mat4{
+            cols: [
+                self.cols[0] - rhs.cols[0],
+                self.cols[1] - rhs.cols[1],
+                self.cols[2] - rhs.cols[2],
+                self.cols[3] - rhs.cols[3],
+            ] 
+        } ;
+    }
+}
+
+impl ops::Mul<Mat4> for Mat4 {
+    type Output = Mat4;
+
+    fn mul(self, rhs: Mat4) -> Mat4 { 
+        let c0 = self.cols[0] * rhs.cols[0].x + self.cols[1] * rhs.cols[0].y + self.cols[2] * rhs.cols[0].z + self.cols[3] * rhs.cols[0].w;
+        let c1 = self.cols[0] * rhs.cols[1].x + self.cols[1] * rhs.cols[1].y + self.cols[2] * rhs.cols[1].z + self.cols[3] * rhs.cols[1].w;
+        let c2 = self.cols[0] * rhs.cols[2].x + self.cols[1] * rhs.cols[2].y + self.cols[2] * rhs.cols[2].z + self.cols[3] * rhs.cols[2].w;
+        let c3 = self.cols[0] * rhs.cols[3].x + self.cols[1] * rhs.cols[3].y + self.cols[2] * rhs.cols[3].z + self.cols[3] * rhs.cols[3].w;
+        return Mat4 {
+            cols: [c0, c1, c2, c3]
+        };
+    }
+}
+
+impl ops::Mul<vec4::Vec4> for Mat4 {
+    type Output = vec4::Vec4;
+
+    fn mul(self, rhs: vec4::Vec4) -> vec4::Vec4 { 
+        return self.cols[0] * rhs.x + self.cols[1] * rhs.y + self.cols[2] * rhs.z + self.cols[3] * rhs.w;
+    }
+}
+
+impl ops::Mul<Mat4> for vec4::Vec4 {
+    type Output = vec4::Vec4;
+
+    fn mul(self, rhs: Mat4) -> vec4::Vec4 { 
+        return vec4::Vec4::new(
+            vec4::Vec4::dot(&self, &rhs.cols[0]),
+            vec4::Vec4::dot(&self, &rhs.cols[1]),
+            vec4::Vec4::dot(&self, &rhs.cols[2]),
+            vec4::Vec4::dot(&self, &rhs.cols[3]),
+        );
+    }
+}
+
+impl ops::Mul<f32> for Mat4 {
+    type Output = Mat4;
+
+    fn mul(self, rhs: f32) -> Mat4 { 
+        return Mat4{
+            cols: [
+                self.cols[0] * rhs,
+                self.cols[1] * rhs,
+                self.cols[2] * rhs,
+                self.cols[3] * rhs,
+            ] 
+        } ;
+    }
+}
+
+impl ops::Mul<Mat4> for f32 {
+    type Output = Mat4;
+
+    fn mul(self, rhs: Mat4) -> Mat4 { 
+        return rhs * self;
+    }
+}
+
+impl ops::Div<f32> for Mat4 {
+    type Output = Mat4;
+
+    fn div(self, rhs: f32) -> Mat4 { 
+        return Mat4{
+            cols: [
+                self.cols[0] / rhs,
+                self.cols[1] / rhs,
+                self.cols[2] / rhs,
+                self.cols[3] / rhs,
+            ] 
+        };
+    }
+}
+
 #[cfg(test)]
 mod test {
     use super::*;
@@ -323,5 +426,139 @@ mod test {
         let expected_col3 = vec4::Vec4::new(1.0, 2.0, 2.0, 1.2);
         let expected = Mat4::from_vec4s(&expected_col0, &expected_col1, &expected_col2, &expected_col3);
         assert!(Mat4::equal_epsilon(&mat, &expected, math::EPSILON_F32_5));
+    }
+
+    #[test]
+    fn test_add() {
+        let lhs = Mat4::new();
+        let rhs = Mat4::from_scalars(
+            1.0, 2.0, 1.0, 4.0,
+            1.0, 3.0, 4.0, 12.1, 
+            1.0, 23.0, 14.0, 12.1, 
+            3.4, 2.3, 4.4, 5.4);
+        let result = lhs + rhs;
+        let expected = Mat4::from_scalars(
+            2.0, 2.0, 1.0, 4.0, 
+            1.0, 4.0, 4.0, 12.1, 
+            1.0, 23.0, 15.0, 12.1, 
+            3.4, 2.3, 4.4, 6.4);
+        assert!(Mat4::equal_epsilon(&result, &expected, math::EPSILON_F32_5));
+    }
+
+    #[test]
+    fn test_sub() {
+        let lhs = Mat4::from_scalars(
+            1.0, 2.0, 1.0, 4.0,
+            1.0, 3.0, 4.0, 12.1, 
+            1.0, 23.0, 14.0, 12.1, 
+            3.4, 2.3, 4.4, 5.4);
+        let rhs = Mat4::new();
+        let result = lhs - rhs;
+        let expected = Mat4::from_scalars(
+            0.0, 2.0, 1.0, 4.0, 
+            1.0, 2.0, 4.0, 12.1, 
+            1.0, 23.0, 13.0, 12.1, 
+            3.4, 2.3, 4.4, 4.4);
+        assert!(Mat4::equal_epsilon(&result, &expected, math::EPSILON_F32_5));
+    }
+
+    #[test]
+    fn test_mul_mat4_mat4() {
+        let c0 = vec4::Vec4::new(2.0, 2.0, 3.0, 4.0);
+        let c1 = vec4::Vec4::new(1.0, 2.0, 2.0, 1.0);
+        let c2 = vec4::Vec4::new(1.0, 2.0, 4.0, 1.0);
+        let c3 = vec4::Vec4::new(1.0, 2.0, 2.0, 1.2);
+        let lhs = Mat4::from_vec4s(&c0, &c1, &c2, &c3);
+
+        let mut axis = vec3::Vec3::new(1.0, 0.0, 1.0);
+        axis = vec3::Vec3::normalize(&axis).unwrap();
+        let angle = math::degree_to_radian(30.0);
+        let mut rhs = Mat4::new();
+        rhs = Mat4::rotate(&rhs, angle, &axis);
+
+        let result = lhs * rhs;
+        let expected_col0 = vec4::Vec4::new(2.2865663, 2.7071072, 3.7740947, 4.1525917);
+        let expected_col1 = vec4::Vec4::new(0.5124718, 1.7320506, 2.0856041, -0.1946352);
+        let expected_col2 = vec4::Vec4::new(0.713434, 1.2928932, 3.225906, 0.8474088);
+        let expected_col3 = vec4::Vec4::new(1.0, 2.0, 2.0, 1.2);
+        let expected = Mat4::from_vec4s(&expected_col0, &expected_col1, &expected_col2, &expected_col3);
+        assert!(Mat4::equal_epsilon(&result, &expected, math::EPSILON_F32_5));
+    }
+
+    #[test]
+    fn test_mul_vec4_mat4() {
+        let lhs = vec4::Vec4::new(2.2, 1.0, 2.4, 1.0);
+        let rhs = Mat4::from_scalars(
+            1.0, 2.0, 1.0, 4.0,
+            1.0, 3.0, 4.0, 12.1, 
+            1.0, 23.0, 14.0, 12.1, 
+            3.4, 2.3, 4.4, 5.4);
+        let result = lhs * rhs;
+        let expected = vec4::Vec4::new(10.6, 26.9, 70.9, 25.74);
+        assert!(vec4::Vec4::equal_epsilon(&result, &expected, math::EPSILON_F32_5));
+    }
+
+    #[test]
+    fn test_mul_mat4_vec4() {
+        let lhs = Mat4::from_scalars(
+            1.0, 2.0, 1.0, 4.0,
+            1.0, 3.0, 4.0, 12.1, 
+            1.0, 23.0, 14.0, 12.1, 
+            3.4, 2.3, 4.4, 5.4);
+        let rhs = vec4::Vec4::new(2.2, 1.0, 2.4, 1.0);
+        let result = lhs * rhs;
+        let expected = vec4::Vec4::new(9.0, 64.9, 44.2, 55.34);
+        assert!(vec4::Vec4::equal_epsilon(&result, &expected, math::EPSILON_F32_5));
+    }
+
+    #[test]
+    fn test_mul_f32_mat4() {
+        let lhs = 2.0;
+        let rhs = Mat4::from_scalars(
+            1.0, 2.0, 1.0, 4.0,
+            1.0, 3.0, 4.0, 12.1, 
+            1.0, 23.0, 14.0, 12.1, 
+            3.4, 2.3, 4.4, 5.4);
+        let result = lhs * rhs;
+        let expected = Mat4::from_scalars(
+            2.0, 4.0, 2.0, 8.0, 
+            2.0, 6.0, 8.0, 24.2, 
+            2.0, 46.0, 28.0, 24.2, 
+            6.8, 4.6, 8.8, 10.8);
+        assert!(Mat4::equal_epsilon(&result, &expected, math::EPSILON_F32_5));
+    }
+
+    #[test]
+    fn test_mul_mat4_f32() {
+        let lhs = Mat4::from_scalars(
+            1.0, 2.0, 1.0, 4.0,
+            1.0, 3.0, 4.0, 12.1, 
+            1.0, 23.0, 14.0, 12.1, 
+            3.4, 2.3, 4.4, 5.4);
+        let rhs = 2.0;
+        let result = lhs * rhs;
+        let expected = Mat4::from_scalars(
+            2.0, 4.0, 2.0, 8.0, 
+            2.0, 6.0, 8.0, 24.2, 
+            2.0, 46.0, 28.0, 24.2, 
+            6.8, 4.6, 8.8, 10.8);
+        assert!(Mat4::equal_epsilon(&result, &expected, math::EPSILON_F32_5));
+    }
+
+    #[test]
+    fn test_div_mat4_f32() {
+        let lhs = Mat4::from_scalars(
+            1.0, 2.0, 1.0, 4.0,
+            1.0, 3.0, 4.0, 12.1, 
+            1.0, 23.0, 14.0, 12.1, 
+            3.4, 2.3, 4.4, 5.4);
+        let rhs = 2.0;
+        let result = lhs / rhs;
+        let expected = Mat4::from_scalars(
+            0.5, 1.0, 0.5, 2.0, 
+            0.5, 1.5, 2.0, 6.05, 
+            0.5, 11.5, 7.0, 6.05, 
+            1.7, 1.15, 2.2, 2.7);
+        assert!(Mat4::equal_epsilon(&result, &expected, math::EPSILON_F32_5));
     }
 }
