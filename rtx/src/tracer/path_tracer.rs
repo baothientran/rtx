19e332ecdb246addaaaf24a2ced2b474f267e50f
Bao Tran <baotran@Baos-MacBook-Pro.local>
2020-12-14 21:49:10 -0500

    remove random sample code for now

--- rtx/src/tracer/path_tracer.rs
+++ rtx/src/tracer/path_tracer.rs
@@ -1,14 +1,11 @@
 use crate::core::image;
-use crate::core::mat4;
 use crate::core::math;
 use crate::core::vec3;
-use crate::core::vec4;
 use crate::scene::camera;
 use crate::scene::ray;
 use crate::scene::world;
-use rand::Rng;
 
-fn path_trace(ray: &ray::Ray, world: &world::World, depth: u32) -> Option<vec3::Vec3> {
+fn path_trace(ray: &ray::Ray, world: &world::World, _depth: u32) -> Option<vec3::Vec3> {
     if let Some(surface) = world.intersect_ray(ray) {
         let mut lo = vec3::Vec3::from(0.0);
         let normal = surface.normal();
@@ -25,35 +22,6 @@ fn path_trace(ray: &ray::Ray, world: &world::World, depth: u32) -> Option<vec3::
                 lo += brdf * li * f32::abs(vec3::Vec3::dot(normal, &wi));
             }
         }
-
-        // // shoot random rays around the scene to sample global lights
-        // let num_samples = 16;
-        // let TBN;
-        // let mut rng = rand::thread_rng();
-        // let mut indirect_lo = vec3::Vec3::from(0.0);
-        // for _i in 0..num_samples {
-        //     let phi: f32 = math::degree_to_radian(rng.gen_range(0.0, 360.0));
-        //     let theta: f32 = math::degree_to_radian(rng.gen_range(0.0, 90.0));
-        //     let sample_position = vec3::Vec3::new(
-        //         f32::sin(theta) * f32::cos(phi),
-        //         f32::cos(theta),
-        //         f32::sin(theta) * f32::sin(phi),
-        //     );
-
-        //     // construct secondary ray to sample color around the scene
-        //     let mut vec4_sample_position = TBN * vec4::Vec4::from_vec3(&sample_position, 1.0);
-        //     sample_position = vec4::Vec4::to_vec3(&vec4_sample_position);
-
-        //     let mut wi = sample_position - surface_point;
-        //     wi = vec3::Vec3::normalize(&wi).unwrap();
-        //     let secondary_ray = ray::Ray::new(surface_point, wi);
-        //     if let Some(li) = path_trace(&secondary_ray, world, depth + 1) {
-        //         let brdf = surface_material.brdf(&surface_point, wo, &wi);
-        //         indirect_lo += brdf * li * f32::abs(vec3::Vec3::dot(normal, &wi));
-        //     }
-        // }
-        // indirect_lo /= num_samples as f32; // may not be correct.
-        // lo += indirect_lo;
 
         // reflection
 
