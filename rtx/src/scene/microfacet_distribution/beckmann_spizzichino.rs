622b9dd6a771fbb192470c56fa1d21b6e10b4a3c
Bao Tran <baotran@Baos-MacBook-Pro.local>
2021-01-16 18:20:22 -0500

    fix Beckmann distribution

--- rtx/src/scene/microfacet_distribution/beckmann_spizzichino.rs
+++ rtx/src/scene/microfacet_distribution/beckmann_spizzichino.rs
@@ -25,7 +25,7 @@ impl microfacet_distribution::MicrofacetDistribution for BeckmannSpizzichino {
         let y_sq = wh.y * wh.y;
         let cos_phi_sq = x_sq / (x_sq + y_sq);
         let sin_phi_sq = 1.0 - cos_phi_sq;
-        let a = cos_phi_sq * self.alpha_x * self.alpha_x + sin_phi_sq * self.alpha_y * self.alpha_y; 
+        let a = cos_phi_sq / (self.alpha_x * self.alpha_x) + sin_phi_sq / (self.alpha_y * self.alpha_y); 
 
         let exp = f32::exp(-tan_theta_sq * a);
         return exp / (math::PI_F32 * self.alpha_x * self.alpha_y * cos_theta_sq * cos_theta_sq);
