dcfb2f453f2fcfedfa8a90311c3beeb564b3d147
Bao Tran <tranthienbao123@gmail.com>
2021-03-05 02:01:06 -0500

    fix cylinder intersection

--- rtx/src/scene/shape/cylinder.rs
+++ rtx/src/scene/shape/cylinder.rs
@@ -4,6 +4,7 @@ use crate::core::vec2;
 use crate::core::vec3;
 use crate::scene::ray;
 use crate::scene::shape;
+use std::mem;
 
 pub struct Cylinder {
     object_to_world: mat4::Mat4,
@@ -44,29 +45,41 @@ impl shape::IntersectableShape for Cylinder {
         let a = d.x * d.x + d.y * d.y;
         let b = 2.0 * (d.x * o.x + d.y * o.y);
         let c = o.x * o.x + o.y * o.y - self.local_radius * self.local_radius;
-
-        let mut ray_time;
         if a == 0.0 {
-            ray_time = -c / b;
-        } else {
-            let discriminant = b * b - 4.0 * a * c;
-            if discriminant < 0.0 {
-                return false;
-            }
-
-            ray_time = (-b + discriminant.sqrt()) / (2.0 * a);
-            if ray_time < 0.0 {
-                ray_time = (-b - discriminant.sqrt()) / (2.0 * a);
-            }
+            return false;
         }
 
+        let discriminant = b * b - 4.0 * a * c;
+        if discriminant < 0.0 {
+            return false;
+        }
+
+        let mut ray_time_0 = (-b + discriminant.sqrt()) / (2.0 * a);
+        let mut ray_time_1 = (-b - discriminant.sqrt()) / (2.0 * a);
+        if ray_time_0 < ray_time_1 {
+            if ray_time_0 < 0.0 {
+                mem::swap(&mut ray_time_0, &mut ray_time_1);
+            }
+        } else if ray_time_1 > 0.0 {
+            mem::swap(&mut ray_time_0, &mut ray_time_1);
+        }
+
+        let mut ray_time = ray_time_0;
         if ray_time < 0.0 {
             return false;
         }
 
-        let local_position = local_ray.calc_position(ray_time);
+        let mut local_position = local_ray.calc_position(ray_time);
         if local_position.z < self.local_z_min || local_position.z > self.local_z_max {
-            return false;
+            ray_time = ray_time_1;
+            if ray_time < 0.0 {
+                return false;
+            }
+
+            local_position = local_ray.calc_position(ray_time);
+            if local_position.z < self.local_z_min || local_position.z > self.local_z_max {
+                return false;
+            }
         }
 
         return ray_time < max_distance;
@@ -79,38 +92,60 @@ impl shape::IntersectableShape for Cylinder {
         let a = d.x * d.x + d.y * d.y;
         let b = 2.0 * (d.x * o.x + d.y * o.y);
         let c = o.x * o.x + o.y * o.y - self.local_radius * self.local_radius;
-
-        let mut ray_time;
         if a == 0.0 {
-            ray_time = -c / b;
-        } else {
-            let discriminant = b * b - 4.0 * a * c;
-            if discriminant < 0.0 {
-                return None;
-            }
-
-            ray_time = (-b + discriminant.sqrt()) / (2.0 * a);
-            if ray_time < 0.0 {
-                ray_time = (-b - discriminant.sqrt()) / (2.0 * a);
-            }
+            return None;
         }
 
+        let discriminant = b * b - 4.0 * a * c;
+        if discriminant < 0.0 {
+            return None;
+        }
+
+        let mut ray_time_0 = (-b + discriminant.sqrt()) / (2.0 * a);
+        let mut ray_time_1 = (-b - discriminant.sqrt()) / (2.0 * a);
+        if ray_time_0 < ray_time_1 {
+            if ray_time_0 < 0.0 {
+                mem::swap(&mut ray_time_0, &mut ray_time_1);
+            }
+        } else if ray_time_1 > 0.0 {
+            mem::swap(&mut ray_time_0, &mut ray_time_1);
+        }
+
+        let mut ray_time = ray_time_0;
         if ray_time < 0.0 {
             return None;
         }
 
-        let local_position = local_ray.calc_position(ray_time);
+        let mut local_position = local_ray.calc_position(ray_time);
         if local_position.z < self.local_z_min || local_position.z > self.local_z_max {
-            return None;
+            ray_time = ray_time_1;
+            if ray_time < 0.0 {
+                return None;
+            }
+
+            local_position = local_ray.calc_position(ray_time);
+            if local_position.z < self.local_z_min || local_position.z > self.local_z_max {
+                return None;
+            }
         }
 
-        let local_normal = vec3::Vec3::new(local_position.x, local_position.y, 0.0);
         let local_dpdu = vec3::Vec3::new(
             -2.0 * math::PI_F32 * local_position.y,
             2.0 * math::PI_F32 * local_position.x,
             0.0,
         );
         let local_dpdv = vec3::Vec3::new(0.0, 0.0, self.local_z_max - self.local_z_min);
+
+        let maybe_local_normal =
+            vec3::Vec3::new(local_position.x, local_position.y, 0.0).normalize();
+        if maybe_local_normal.is_none() {
+            return None;
+        }
+
+        let mut local_normal = maybe_local_normal.unwrap();
+        if local_ray.direction().dot(&local_normal) > 0.0 {
+            local_normal = -local_normal;
+        }
 
         return Some(shape::IntersectableShapeSurface::new(
             ray_time,
